3ac4649c02648caa6b898b78ff0ccb970a29f180
Jerome <jerome@MacBook-Pro-de-Jerome.local>
2021-11-23 22:08:35 +0100

    createServer+achitecture

---
+++ src/config/server.js
@@ -0,0 +1,31 @@
+class Server {
+    
+    #http;
+    constructor(http) {
+        this.#http = http;
+    }
+
+    middlewares(middlewares) {
+        for (const key in middlewares) {
+            this.#http.use(middlewares[key]);
+        }
+    }
+
+    routes(routes) {
+        for (const path in routes) {
+            this.#http.use(path, routes[path]);
+        }
+    }
+
+    errorHandler(errorHandler) {
+        this.#http.use(errorHandler);
+    }
+
+    start(port) {
+        this.#http.listen(port, () => {
+            console.log(`server started on port  ${port}`);
+        })
+    }
+}
+
+export default Server;
